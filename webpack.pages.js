46016782f77e4bcd8fba1cc2418c2b3f796afe1a
Matthias Dumke <matthias.dumke@gmx.net>
2021-04-10 20:48:29 +0200

    change public path

--- webpack.pages.js
+++ webpack.pages.js
@@ -32,6 +32,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'docs'),
+    publicPath: '/area-guide',
     assetModuleFilename: '[name][ext]',
     clean: true
   },
