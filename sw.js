dd577315af8f66824020a5e55df837404d3b760d
Matthias Dumke <matthias.dumke@gmx.net>
2021-04-11 14:34:09 +0200

    update cache version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // bump the version cached *assets* change
-const STATIC_CACHE = 'static-v1'
-const DYNAMIC_CACHE = 'dynamic-v1'
+const STATIC_CACHE = 'static-v2'
+const DYNAMIC_CACHE = 'dynamic-v2'
 
 self.addEventListener('install', event => {
   // cache selected requests
